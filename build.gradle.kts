c64625ba639a440c6525c9a705194544481ae80c
Christophe Beyls <software@digitalia.be>
2024-05-16 12:09:37 +0200

    rename kotlin-serialization to kotlinx-serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   alias(libs.plugins.android.test) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.parcelize) apply false
-  alias(libs.plugins.kotlin.serialization) apply false
+  alias(libs.plugins.kotlinx.serialization) apply false
   alias(libs.plugins.ksp) apply false
   alias(libs.plugins.hilt.plugin) apply false
   alias(libs.plugins.spotless)

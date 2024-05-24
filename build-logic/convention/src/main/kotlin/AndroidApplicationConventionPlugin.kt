1c862b87957707db95144b400122d22be9e9c47f
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-24 12:02:05 +0900

    [refactor/kotlinoptions-to-compileroptions] Migrate kotlinOptions to CompilerOptions
    - Due to kotlinOptions function is depreacted at Kotlin 2.0

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -3,6 +3,9 @@ import com.skydoves.pokedex.compose.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
@@ -15,6 +18,29 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
       extensions.configure<BaseAppModuleExtension> {
         configureKotlinAndroid(this)
         defaultConfig.targetSdk = 34
+      }
+
+      extensions.getByType<KotlinAndroidProjectExtension>().apply {
+        compilerOptions {
+          allWarningsAsErrors.set(
+            properties["warningsAsErrors"] as? Boolean ?: false
+          )
+
+          freeCompilerArgs.set(
+            freeCompilerArgs.getOrElse(emptyList()) + listOf(
+              "-Xcontext-receivers",
+              "-Xopt-in=kotlin.RequiresOptIn",
+              // Enable experimental coroutines APIs, including Flow
+              "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+              // Enable experimental compose APIs
+              "-Xopt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+              "-Xopt-in=androidx.lifecycle.compose.ExperimentalLifecycleComposeApi",
+              "-Xopt-in=androidx.compose.animation.ExperimentalSharedTransitionApi",
+            )
+          )
+
+          jvmTarget.set(JvmTarget.JVM_17)
+        }
       }
     }
   }
--- build-logic/convention/src/main/kotlin/com/skydoves/pokedex/compose/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/skydoves/pokedex/compose/KotlinAndroid.kt
@@ -3,11 +3,6 @@ package com.skydoves.pokedex.compose
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionAware
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 
 /**
  * Configure base Kotlin with Android options
@@ -30,28 +25,5 @@ internal fun Project.configureKotlinAndroid(
     lint {
       abortOnError = false
     }
-
-    kotlinOptions {
-      // Treat all Kotlin warnings as errors (disabled by default)
-      allWarningsAsErrors = properties["warningsAsErrors"] as? Boolean ?: false
-
-      freeCompilerArgs = freeCompilerArgs + listOf(
-        "-Xcontext-receivers",
-        "-opt-in=kotlin.RequiresOptIn",
-        // Enable experimental coroutines APIs, including Flow
-        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-        // Enable experimental compose APIs
-        "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
-        "-opt-in=androidx.lifecycle.compose.ExperimentalLifecycleComposeApi",
-        "-opt-in=androidx.compose.animation.ExperimentalSharedTransitionApi",
-      )
-
-      // Set JVM target to 17
-      jvmTarget = JavaVersion.VERSION_17.toString()
-    }
   }
-}
-
-fun CommonExtension<*, *, *, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-  (this as ExtensionAware).extensions.configure("kotlinOptions", block)
 }

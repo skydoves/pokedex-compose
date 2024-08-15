9c8b98b6402b1313a3814ea3acb7cce02cebccdf
skydoves <skydoves@naver.com>
2024-08-15 17:46:25 +0900

    Release 1.0.3

--- buildSrc/src/main/kotlin/com/skydoves/pokedex/compose/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/pokedex/compose/Configuration.kt
@@ -3,7 +3,7 @@ package com.skydoves.pokedex.compose
 object Configuration {
   const val majorVersion = 1
   const val minorVersion = 0
-  const val patchVersion = 2
+  const val patchVersion = 3
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 3
+  const val versionCode = 4
 }

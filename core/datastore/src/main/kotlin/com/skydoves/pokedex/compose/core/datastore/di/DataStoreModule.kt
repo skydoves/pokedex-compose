e02cf2df3c193f7d2967e9b50d3b5abecb9097da
hmzgtl16 <hmzgtl16@gmail.com>
2025-11-27 20:41:52 +0100

    chore: Add license headers and apply formatting
    Adds the Apache 2.0 license header to files in the `:core:datastore` module. This commit also includes minor code formatting adjustments.

--- core/datastore/src/main/kotlin/com/skydoves/pokedex/compose/core/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/kotlin/com/skydoves/pokedex/compose/core/datastore/di/DataStoreModule.kt
@@ -1,3 +1,19 @@
+/*
+ * Designed and developed by 2024 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.skydoves.pokedex.compose.core.datastore.di
 
 import android.content.Context
@@ -26,13 +42,13 @@ object DataStoreModule {
   @Singleton
   fun providesUserPreferencesDataStore(
     @ApplicationContext context: Context,
-    @Dispatcher(PokedexAppDispatchers .IO) dispatcher: CoroutineDispatcher,
+    @Dispatcher(PokedexAppDispatchers.IO) dispatcher: CoroutineDispatcher,
     @PokedexAppScope scope: CoroutineScope,
     userPreferencesSerializer: UserPreferencesSerializer,
   ): DataStore<UserPreferences> = DataStoreFactory
     .create(
       serializer = userPreferencesSerializer,
       scope = CoroutineScope(scope.coroutineContext + dispatcher),
-      produceFile = { context.dataStoreFile(fileName = "user_preferences.pb") }
+      produceFile = { context.dataStoreFile(fileName = "user_preferences.pb") },
     )
 }
